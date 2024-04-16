1abe2e531b81e1092bfbd8104e054cbf092c9d8c
Nadeem Algobany <Algobany33@gmail.com>
2024-04-15 22:40:34 -0400

    2

--- finalproject200/blackjack.cpp
+++ finalproject200/blackjack.cpp
@@ -4,3 +4,13 @@
 #include <random>
 #include <ctime>
 #include <map
+class Card {
+public:
+    enum Rank { TWO = 2, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK = 10, QUEEN = 10, KING = 10, ACE = 11 };
+    enum Suit { HEARTS, DIAMONDS, CLUBS, SPADES };
+
+    Card(Rank r, Suit s) : rank(r), suit(s) {}
+
+    int getValue() const {
+        return rank;
+    }
