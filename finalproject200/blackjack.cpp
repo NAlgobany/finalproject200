fcdbb2c5fc2646473cf0f5862fec43e9982ee6cc
Nadeem Algobany <Algobany33@gmail.com>
2024-04-22 23:04:05 -0400

    day 9

--- finalproject200/blackjack.cpp
+++ finalproject200/blackjack.cpp
@@ -22,7 +22,7 @@ private:
 };
 
 class Deck {
-public
+public:
     std::vector<Card> cards;
 
     Deck() {
@@ -44,7 +44,7 @@ class Shoe {
 public:
     std::vector<Card> cards;
 
-    Shoe(int numDecks = 6) { 
+    Shoe(int numDecks = 6) {
         for (int i = 0; i < numDecks; ++i) {
             Deck deck;
             deck.shuffle();
@@ -76,3 +76,71 @@ public:
         cards.push_back(card);
     }
 
+    int getValue() const {
+        int totalValue = 0;
+        int aceCount = 0;
+        for (const Card& card : cards) {
+            totalValue += card.getValue();
+            if (card.getValue() == Card::ACE) {
+                aceCount++;
+            }
+        }
+        while (totalValue > 21 && aceCount > 0) {
+            totalValue -= 10; 
+            aceCount--;
+        }
+        return totalValue;
+    }
+};
+
+class BlackjackGame {
+public:
+    Shoe shoe;
+    int wallet = 0;
+
+    void playHand(bool playerHits) {
+        Hand playerHand, dealerHand;
+        playerHand.addCard(shoe.dealCard());
+        playerHand.addCard(shoe.dealCard());
+        dealerHand.addCard(shoe.dealCard());
+        dealerHand.addCard(shoe.dealCard());
+
+        if (playerHits) {
+            playerHand.addCard(shoe.dealCard());
+        }
+
+       
+        while (dealerHand.getValue() < 17) {
+            dealerHand.addCard(shoe.dealCard());
+        }
+
+        int playerScore = playerHand.getValue();
+        int dealerScore = dealerHand.getValue();
+
+        if ((playerScore > 21) || (playerScore < dealerScore && dealerScore <= 21)) {
+            wallet--;
+        }
+        else if (playerScore == dealerScore) {
+           
+        }
+        else {
+            wallet++;
+        }
+    }
+};
+
+int main() {
+    BlackjackGame game;
+    std::map<std::pair<int, bool>, std::pair<int, int>> results; 
+
+    for (int i = 0; i < 100000; ++i) {
+        bool hit = rand() % 2 == 0; 
+        int startingWallet = game.wallet;
+        game.playHand(hit);
+        int result = game.wallet - startingWallet;
+        int playerStartingValue = game.shoe.cards.back().getValue(); 
+
+        if (result > 0) {
+            results[{playerStartingValue, hit}].first++; 
+        }
+        else
